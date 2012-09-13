c285f34519ca884f361e4167f80ca56eda2781bb
Xfurry <xfurry@scriptdev2.com>
2012-09-13 23:08:46 +0300

    Update quest 9303

--- Updates/1.2.4_updatepack_dev.sql
+++ Updates/1.2.4_updatepack_dev.sql
@@ -1,8 +1,9 @@
 -- Item update
-delete from item_required_target where entry in (22783,22784);
+delete from item_required_target where entry in (22783,22784,22962);
 insert into item_required_target values
 (22784,1,16329),
-(22783,1,16329);
+(22783,1,16329),
+(22962,1,16518);
 
 
 delete from conditions where condition_entry in (717,718);
@@ -13,8 +14,9 @@ update gameobject_loot_template set condition_id=717 where entry=2774 and item=1
 update gameobject_loot_template set condition_id=718 where entry=2774 and item=12768;
 
 UPDATE creature_template SET spell1=29992 WHERE entry=17178;
-UPDATE quest_template SET ReqSpellCast1=0 WHERE entry IN (6129,6124);
+UPDATE quest_template SET ReqSpellCast1=0 WHERE entry IN (6129,6124,9303);
 
 UPDATE quest_template SET ReqCreatureOrGOId1=12299 WHERE entry=6124;
 UPDATE quest_template SET ReqCreatureOrGOId1=12297 WHERE entry=6129;
+UPDATE quest_template SET ReqCreatureOrGOId1=16534 WHERE entry=9303;
 
