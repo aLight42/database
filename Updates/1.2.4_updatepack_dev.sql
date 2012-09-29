902be99c181ab8f70269cbd386078429c86a5871
Xfurry <xfurry@scriptdev2.com>
2012-09-29 14:16:03 +0300

    Some quest fixes

--- Updates/1.2.4_updatepack_dev.sql
+++ Updates/1.2.4_updatepack_dev.sql
@@ -20,10 +20,39 @@ UPDATE quest_template SET ReqCreatureOrGOId1=12299 WHERE entry=6124;
 UPDATE quest_template SET ReqCreatureOrGOId1=12297 WHERE entry=6129;
 UPDATE quest_template SET ReqCreatureOrGOId1=16534 WHERE entry=9303;
 
--- Varimathras
+-- Varimathras (missing)
 DELETE FROM creature WHERE id=2425;
-INSERT INTO creature (`id`,`map`,`spawnMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`) VALUES
+INSERT INTO creature (id,map,spawnMask,modelid,equipment_id,position_x,position_y,position_z,orientation,spawntimesecs,spawndist,currentwaypoint,curhealth,curmana,DeathState,MovementType) VALUES
 (2425, 0, 1, 0, 0, 1287.7, 333.589, -59.9998, 0.959931, 180, 0, 0, 18900, 0, 0, 0);
 
+-- Creature_template fixes (UDB)
 UPDATE creature_template SET spell1=42464 WHERE entry=23811;
+UPDATE creature_template SET mindmg=42, maxdmg=56, attackpower=102, minrangedmg=29, maxrangedmg=42, rangedattackpower=9 WHERE entry=3414;
+UPDATE creature_template SET maxlevel=42 WHERE entry=5237;
+UPDATE creature_template SET armor=1179 WHERE entry=4104;
+
+
+-- quest 5581
+-- Script id: 177398
+DELETE FROM gameobject_template_scripts WHERE id=177398;
+INSERT INTO gameobject_template_scripts VALUES 
+(177398,1,10,11937,60000,0,0,0,0,0,0,0,0,0,0,0,'Demon Portal - Summon Demon Portal Guardian');
+UPDATE gameobject_template SET data3=3000 WHERE entry=177398;
+
+
+-- quest 2987 (most likely not yet blizzlike)
+-- ToDo: missing eff idx 1 (summon go 177683). Also, this needs to be moved in core
+DELETE FROM spell_template WHERE id IN (11756);
+INSERT INTO spell_template VALUES
+(11756, 0x00000000, 101,  4,  76,  47,   15,   0, 144064, 0, 'Summon Gordunni chest (COBALT)');
+
+-- spell script target
+delete from spell_script_target where entry=11757;
+insert into spell_script_target values
+(11757,0,144050);
+-- Script id: 19395
+DELETE FROM spell_scripts WHERE id=19395;
+INSERT INTO spell_scripts VALUES 
+(19395,0,15,11756,0,0,0,0,0,0,0,0,0,0,0,0,'Gordunni Trap - Cast Summon Gordunni chest (COBALT)');
+
 
